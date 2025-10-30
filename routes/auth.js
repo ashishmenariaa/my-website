abf9c2f354593740ffcdce2265c43e046d65a885
Ashish <ashishmenaria73@gmail.com>
2025-10-30 22:47:48 +0530

    your commit message

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,4 @@
+//auth.js in routes
 const express = require('express');
 const User = require('../models/user');
 const { generateToken, setTokenCookie, authenticate } = require('../middleware/auth');
